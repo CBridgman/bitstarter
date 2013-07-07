4079edcbe041339153fc044d90fddc784f6d8020
Cadyn Bridgman <cbridgman@gmail.com>
2013-07-07 19:45:05 +0000

    web.js now loads index.html, added .gitignore file

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(loadFile('index.html'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var loadFile = function (fileName) {
+  var fileBuffer = fs.readFileSync(fileName);
+  return fileBuffer.toString('utf8');
+}
