6b12810cc7c555cebd70bbc1dc4bdf5e3c3c939c
Cadyn Bridgman <cbridgman@gmail.com>
2013-07-08 01:50:30 +0000

    Upgraded grader.js to accept a url for the html file, and added additional checks to checks.json

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ Reference:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -36,17 +37,13 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
-
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile);
+var checkHtmlFile = function(htmlFileData, checksFile) {
+    $ = cheerio.load(htmlFileData);
+    var checks = loadChecks(checksFile);
     var out = {};
     for (var ii in checks) {
 	var present = $(checks[ii]).length > 0;
@@ -61,14 +58,33 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var processHtmlFile = function(htmlFileData, checksFile) {
+    var checkJson = checkHtmlFile(htmlFileData, checksFile);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.url != null) {
+	//console.log('Grading File at URL: ' + program.url);
+	rest.get(program.url).on('complete', function(result) {
+	    if (result instanceof Error) {
+		console.log("Error retrieving URL: %s. Exiting.", result.message);
+		process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	    }
+
+	    processHtmlFile(result, program.checks);
+	});
+    } else {
+	var htmlFileData = fs.readFileSync(program.file);
+	processHtmlFile(htmlFileData, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
